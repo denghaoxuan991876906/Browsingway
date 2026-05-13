26e5da44c537d27a1ae4a69e5952c52bed4084bb
嗨呀www <991876906@qq.com>
2026-05-13 20:37:50 +0800

    fix: remove unnecessary 15s cooldown, Reload resets paint timer

--- Browsingway.Renderer/Overlay.cs
+++ Browsingway.Renderer/Overlay.cs
@@ -17,7 +17,6 @@ internal class Overlay : IDisposable
 	public readonly TextureRenderHandler RenderHandler;
 	private ChromiumWebBrowser? _browser;
 	private Timer? _paintWatchTimer;
-	private long _lastWatchdogReload;
 	private string _url;
 	private float _zoom;
 	private bool _muted;
@@ -93,11 +92,9 @@ internal class Overlay : IDisposable
 		{
 			long now = DateTimeOffset.Now.ToUnixTimeMilliseconds();
 			long since = now - RenderHandler.LastPaintTime;
-			long sinceLastReload = now - _lastWatchdogReload;
-			if (since > 5000 && sinceLastReload > 15000)
+			if (since > 5000)
 			{
 				Console.WriteLine($"[PaintWatch] {_id}: no paint for {since}ms, reloading");
-				_lastWatchdogReload = now;
 				RenderHandler.ResetPaintTime();
 				_browser?.Reload();
 			}
